c31248bf6794118fa0de892d9dfa6ad8b1a05472
youwei.wang <your.email@example.com>
2026-02-26 15:53:41 +0800

    Update extracted_images

---
+++ git_upload.sh
@@ -0,0 +1,16 @@
+# git init 
+cd /Users/youwei.wang/Documents/PythonProject/image_repo/extracted_images \
+&& git remote add origin https://github.com/russwestbrick/image_cloud.git
+
+
+# upload to new branch
+git checkout -b 'image_testset_v1' \
+&& git add . \
+&& git commit -m "Add all_accounts_from_offline_db" \
+&& git push -u origin 'image_testset_v1'
+
+# overwrite to existing branch
+git checkout 'image_testset_v1' \
+&& git add -A \
+&& git commit -m "Update extracted_images" \
+&& git push -f
